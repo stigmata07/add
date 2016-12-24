45dd7824f975b426c72a07ff19858c64e707dc76
stigmata07 <suresh.stigmata07@gmail.com>
2016-12-24 15:48:09 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author GodsGift
+ */
+public class add {
+    public static int a=10;
+     public static int b=10; 
+     public static int c=a+b;
+    public static void main(String[] args) {
+        System.out.println("A+B ="+c);
+    }
+    
+}
